47d4be82e4a043122f5127daa39fd1230ff71255
OhKannaDuh <uimokd@gmail.com>
2025-04-26 07:49:57 +0100

    StellarMission version bump (2.1.2)

--- Ferret/Templates/StellarMissions.lua
+++ Ferret/Templates/StellarMissions.lua
@@ -15,7 +15,7 @@ function StellarMissions:new()
     self.medicine_to_drink = nil;
     self.food_to_eat = nil;
     self.job = nil;
-    self.template_version = Version:new(2, 1, 1);
+    self.template_version = Version:new(2, 1, 2);
 end
 
 function StellarMissions:setup()
